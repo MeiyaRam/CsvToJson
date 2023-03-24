dcc5c543601e3c2a47258ebd5f961c6736afc912
MeiyaRam <MeiyaRam@gmail.com>
2023-03-24 11:07:33 +0530

    Create minimum and maximum status

--- shops/shopDetails.js
+++ shops/shopDetails.js
@@ -1,20 +1,44 @@
 const renderData = require('./data.js');
 
-const getItemDetails = (shopItems) => shopItems.map((item) => item.name);
+const getMinimumStatus = (array, currentPrice) => {
+   const minPrice = array.reduce((acc, cur) =>
+      !Number(cur.price) || (acc < cur.price) ? acc : cur.price);
+   const minStatus = (minPrice == currentPrice) ? true : false;
+   
+   return minStatus;
+}
 
-const getItemPrice = (shopItems, uniqueName) => {
-   const price = (shopItems.find((item) => item.name === uniqueName) || {price:'-'})?.price;
+const getMaximumStatus = (array, currentPrice) => {
+   const maxPrice = array.reduce((acc, cur) =>
+      !Number(cur.price) || (acc > cur.price) ? acc : cur.price, 0);
+   const maxStatus = (maxPrice == currentPrice) ? true : false;
 
+   return maxStatus;
+}
+
+const getShopPriceDetails = ({ price }, index, array) => {
    return {
-      price,
-      minimum: true,
-      maximum: false
+      price: price,
+      minimum: getMinimumStatus(array, price),
+      maximum: getMaximumStatus(array, price),
+      
    };
 }
 
+const getItemDetails = (shopItems) => shopItems.map((item) => item.name);
+
+const getItemPrice = (shopItems, uniqueName) => {
+   const price = (shopItems.find((item) => item.name === uniqueName) || { price: "-" })?.price;
+
+   return { price };
+}
+
+const getShopPrices = (uniqueName) => renderData.map((shopItems) =>
+   getItemPrice(shopItems.items, uniqueName));
+
 const display = (renderData) => {
-   const shopNames = renderData.map((shop) =>shop.shopName);
-   
+   const shopNames = renderData.map((shop) => shop.shopName);
+
    const shopItems = renderData.map((shop) =>
       getItemDetails(shop.items)
    ).flat();
@@ -23,10 +47,10 @@ const display = (renderData) => {
 
    const items = uniqueItem.map((uniqueName) => ({
       name: uniqueName,
-      shopPrices: renderData.map((shopItems) =>
-         getItemPrice(shopItems.items, uniqueName))
+      shopPrices: getShopPrices(uniqueName).map(getShopPriceDetails),
+
    }));
-   
+
    return { shopNames, items };
 }
 
